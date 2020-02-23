efb0015c2251fe10088e41e30645784622622c8c
Stanislav Hrytcyshyn <stashrutc@ukr.net>
2020-02-23 11:41:03 +0200

    Created structure for domria scrapper. Created logic of scrapping with pagination.

--- service/scrappers/dom_ria_scrapper.py
+++ service/scrappers/dom_ria_scrapper.py
@@ -1,17 +1,20 @@
 from service.scrappers.base_scrapper import BaseScrapper, BeautifulSoup
-from bs4.element import Tag
+from bs4.element import Tag, ResultSet
 from collections import namedtuple
+from functools import partialmethod
 import re
+
+from constants import MAXIMUM_AMOUNT_OF_PAGES_FOR_SCRAPPING
+
 
 class DomRiaScrapper(BaseScrapper):
     """A class for scrapping domria resource
     """
 
-    def __init__(self, url: str="https://dom.ria.com/uk/arenda-kvartir/lvov/?page=1",
-                 name: str="DomRia listings") -> None:
+    def __init__(self, url: str = "https://dom.ria.com/uk/arenda-kvartir/lvov/?page=1",
+                 name: str = "DomRia listings") -> None:
 
         super().__init__(url, name)
-        self.dom_ria_soup = BaseScrapper.create_soup_obj(url)
         self.catalog_class = "ticket-clear line"
         self.realty_inner_link_class = "realtyPhoto"
         self.location_info_class = "blue"
@@ -19,7 +22,7 @@ class DomRiaScrapper(BaseScrapper):
         self.inner_meta_feature_class = "label grey"
         self.inner_meta_feature_value_class = "indent"
 
-    def parse_meta(self, meta_keys, meta_values):
+    def parse_meta(self, meta_keys: ResultSet, meta_values: ResultSet) -> dict:
 
         meta_titles = [el.text.strip() for el in list(meta_keys)]
         meta_values = [el.text.strip() for el in list(meta_values)]
@@ -65,25 +68,78 @@ class DomRiaScrapper(BaseScrapper):
         if not isinstance(scrapped_data, Tag):
             raise TypeError("type of scrapped_data should be Tag (row html markup)")
 
-        img_src = scrapped_data.find("img")["src"]
+        try:
+            img_src = scrapped_data.find("img")["src"]
+        except KeyError as err:
+            print("Img has no attribute src.", err)
+            img_src = scrapped_data.find("img")["data-src"]
+
         inner_link = scrapped_data.find("a", attrs={"class": re.compile(f"{self.realty_inner_link_class}.+$")})["href"]
 
-        location = scrapped_data.find("a", attrs={"class": "blue"}).getText().strip()
-
-        listing_location = self.parse_location(location)
-
-        listing_price = self.parse_price(scrapped_data.find("b", attrs={"class": self.house_price_class}).getText())
+        scrapped_location = scrapped_data.find("a", attrs={"class": "blue"}).getText().strip()
+        scrapped_price = scrapped_data.find("b", attrs={"class": self.house_price_class}).getText()
 
         inner_content_soup = BaseScrapper.create_soup_obj(self.parse_url(inner_link))
+        scrapped_feature = inner_content_soup.find_all("div", attrs={"class": self.inner_meta_feature_class})
+        scrapped_feature_value = inner_content_soup.find_all("div", attrs={"class": self.inner_meta_feature_value_class})
 
-        listing_feature = inner_content_soup.find_all("div", attrs={"class": self.inner_meta_feature_class})
-        listing_feature_value = inner_content_soup.find_all("div", attrs={"class": self.inner_meta_feature_value_class})
+        listing_location = self.parse_location(scrapped_location)
+        listing_price = self.parse_price(scrapped_price)
 
-        structured_meta = self.parse_meta(listing_feature, listing_feature_value)
+        structured_meta = self.parse_meta(scrapped_feature, scrapped_feature_value)
+
+        result["price"] = listing_price
+        result["img"] = img_src
+        print(listing_location)
+        result["district"], result["street"] = listing_location.district, listing_location.street
+        result["meta"] = structured_meta
 
         return result
 
 
+    def paginate_page(self, current_page_url: str) -> str:
+        page_index = current_page_url.find("=") + 1
+        current_page_number = current_page_url[page_index:]
+
+        return current_page_url.replace(current_page_number, str(int(current_page_number) + 1))
+
+
+    def general_scrapp(self):
+        resulted_listings = []
+        domria_url = self._source_url
+
+        try:
+            page_soup = BaseScrapper.create_soup_obj(domria_url)
+        except TypeError:
+            print("Incorrect url for creating bs object.")
+
+        for _ in range(MAXIMUM_AMOUNT_OF_PAGES_FOR_SCRAPPING):
+            catalog = page_soup.find_all("section", attrs={"class": re.compile(f"{self.catalog_class}.+$")})
+
+            for listing in catalog:
+                try:
+                    listing_info = self.scrap_single_listing(listing)
+                    resulted_listings.append(listing_info)
+                except ValueError as err:
+                    print(err)
+                    continue
+
+            domria_url = self.paginate_page(domria_url)
+            page_soup = BaseScrapper.create_soup_obj(domria_url)
+        else:
+            print("No more pages left.")
+            return resulted_listings
+
 
     def __repr__(self):
         super().__repr__()
+
+
+if __name__ == "__main__":
+    domria_scrapper = DomRiaScrapper()
+
+    print("start")
+    listings = domria_scrapper.general_scrapp()
+
+    print(listings)
+
